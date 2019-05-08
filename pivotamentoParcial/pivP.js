b16dd14582b7db752dba0e1bcb5989022d92c255
LEONARDO DE ANDRADE OLIVEIRA <leonardo.oliveira@mpap.local>
2019-05-08 12:04:45 -0300

    adcionando a fun troca de linha e iteracoes

--- pivotamentoParcial/pivP.js
+++ pivotamentoParcial/pivP.js
@@ -1,25 +1,33 @@
-let matriz = [[3, 5, 6], [7, 8, 9], [10, 2, 4]];
+let matriz = [[3, 5, 6, 2], [7, 8, 9, -3], [10, 2, 4, 4]];
 let b = [2, -3, 4];
 
 function verificaPivo(A) {
   let maiorValor,
     linhaP,
     antigaLinha = null;
-  const A = A;
-
+  let M = A;
+  let mIK;
   for (i in A) {
+    maiorValor = A[i][i];
     for (j in A) {
-      maiorValor = A[i][i];
       antigaLinha = i;
-      if (maiorValor > A[j][i]) {
+      if (maiorValor < A[j][i]) {
         maiorValor = A[j][i];
         linhaP = j;
       }
     }
-    if (maiorValor != A[i][i]) {
-      A = trocaLinha(A, linhaP, antigaLinha);
+    if (maiorValor != A[i][i] && i < A.length - 1) {
+      M = trocaLinha(A, linhaP, antigaLinha);
+    }
+    for (let a = i + 1; a < M.length; a++) {
+      mIK = M[a][i] / maiorValor;
+      for (let b = i + 1; b < M[i].length; b++) {
+        M[a][b] = M[a][b] - mIK * M[i][b];
+        M[a][i] = 0;
+      }
     }
   }
+  //return M;
 }
 
 function trocaLinha(A, linhaP, antigaLinha) {
@@ -28,3 +36,6 @@ function trocaLinha(A, linhaP, antigaLinha) {
   A[linhaP] = aux;
   return A;
 }
+//console.log(trocaLinha(matriz, 2, 0));
+
+verificaPivo(matriz);
