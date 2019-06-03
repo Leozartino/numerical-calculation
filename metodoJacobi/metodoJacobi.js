f17af8ef2efa1425324a221064fc781ea6f48f9e
LEONARDO DE ANDRADE OLIVEIRA <leonardo.oliveira@mpap.local>
2019-06-03 12:17:16 -0300

    adicionando metodo para veirificar integri e etc

--- metodoJacobi/metodoJacobi.js
+++ metodoJacobi/metodoJacobi.js
@@ -1,50 +1,56 @@
 const math = require("mathjs");
-const verificaSistema = require("./verificaSistema");
-const verificaSistemaLinear = verificaSistema.verificaSistemaLinear;
-const diagonalDominante = require("./diagonalDominante");
-const diagonalD = diagonalDominante.diagonalD;
+const verificaSistemaLinear = require("./verificaSistema")
+  .verificaSistemaLinear;
+const diagonalD = require("./diagonalDominante").diagonalD;
+const verificaInte = require("./verificaIntegridade").verificaInte;
 
 function gaussJacobi(A, b, taxaDeErro, numeroMaxIteracoes, chuteInicial) {
-  console.log(diagonalD(A));
+  const resp = verificaInte(A, b, taxaDeErro, numeroMaxIteracoes, chuteInicial);
+  if (resp === 1) {
+    console.log(diagonalD(A));
 
-  let solucaoAnterior = [];
-  let solucaoAtual = [];
-  for (let k = 0; k < b.length; k++) {
-    solucaoAnterior[k] = chuteInicial[k]; //Math.floor((Math.random() * 10000) + 1);
-  }
-
-  let contadorDeIteracoes = 0;
-  let criterioDeParada = false;
-
-  while (
-    criterioDeParada === false &&
-    contadorDeIteracoes < numeroMaxIteracoes
-  ) {
-    for (let i = 0; i < b.length; i++) {
-      let soma = 0;
-      for (let j = 0; j < b.length; j++) {
-        if (j != i) {
-          soma = soma + (A[i][j] * solucaoAnterior[j]) / A[i][i];
-        }
-        solucaoAtual[i] = b[i] / A[i][i] - soma;
-      }
+    let solucaoAnterior = [];
+    let solucaoAtual = [];
+    for (let k = 0; k < b.length; k++) {
+      solucaoAnterior[k] = chuteInicial[k]; //Math.floor((Math.random() * 10000) + 1);
     }
-    //console.log(Math.abs(math.norm(solucaoAtual) - math.norm(solucaoAnterior)));
-    if (
-      Math.abs(math.norm(solucaoAtual) - math.norm(solucaoAnterior)) <=
-      taxaDeErro
+
+    let contadorDeIteracoes = 0;
+    let criterioDeParada = false;
+
+    while (
+      criterioDeParada === false &&
+      contadorDeIteracoes < numeroMaxIteracoes
     ) {
-      criterioDeParada = true;
-    } else {
-      solucaoAnterior = solucaoAtual.slice(0);
+      for (let i = 0; i < b.length; i++) {
+        let soma = 0;
+        for (let j = 0; j < b.length; j++) {
+          if (j != i) {
+            soma = soma + (A[i][j] * solucaoAnterior[j]) / A[i][i];
+          }
+          solucaoAtual[i] = b[i] / A[i][i] - soma;
+        }
+      }
+      //console.log(Math.abs(math.norm(solucaoAtual) - math.norm(solucaoAnterior)));
+      if (
+        Math.abs(math.norm(solucaoAtual) - math.norm(solucaoAnterior)) <=
+        taxaDeErro
+      ) {
+        criterioDeParada = true;
+      } else {
+        solucaoAnterior = solucaoAtual.slice(0);
+      }
+      contadorDeIteracoes += 1;
     }
-    contadorDeIteracoes += 1;
+
+    return solucaoAtual;
+  } else if (resp === 0) {
+    return resp;
   }
-  return solucaoAtual;
 }
 
-let matrizA = [[3, 2, -1], [1, 9, 2], [2, 8, 9]];
-let vetorB = [-9, -2, 2];
+let matrizA = [[5, 3, -2], [10, 9, 2], [2, 6, 9]];
+let vetorB = ["5", -5, -6];
 let taxaDeErro = 0;
 let numeroMaxIteracoes = 100000;
 const solucao = gaussJacobi(matrizA, vetorB, taxaDeErro, numeroMaxIteracoes, [
@@ -53,7 +59,10 @@ const solucao = gaussJacobi(matrizA, vetorB, taxaDeErro, numeroMaxIteracoes, [
   0
 ]);
 
-console.log("\nO vetor solução da última iteração é:\n");
-console.log(solucao);
-
-console.log(verificaSistemaLinear(matrizA, solucao, vetorB));
+if (Array.isArray(solucao)) {
+  console.log("\nO vetor solução da última iteração é:\n");
+  console.log(solucao);
+  console.log(verificaSistemaLinear(matrizA, solucao, vetorB));
+} else {
+  console.log("Sem solução");
+}
---
+++ metodoJacobi/verificaIntegridade.js
@@ -0,0 +1,61 @@
+function verificaInte(A, b, taxaDeErro, numeroMaxIteracoes, chuteInicial) {
+  let TesteCondicional = null;
+  if (Array.isArray(A)) {
+    for (let i = 0; i < A.length; i++) {
+      for (let j = 0; j < A.length; j++) {
+        if (typeof A[i][j] != "number") {
+          console.log(`O valor: ${A[i][j]} não é um valor válido!`);
+          TesteCondicional = true;
+        }
+      }
+    }
+  } else if (!Array.isArray(A)) {
+    console.log("Erro! Informe um array válido!");
+    TesteCondicional = true;
+  }
+
+  if (Array.isArray(b)) {
+    for (let x = 0; x < A.length; x++) {
+      if (typeof b[x] != "number") {
+        console.log(`O valor: ${b[x]} não é um valor válido!`);
+        TesteCondicional = true;
+      }
+    }
+  } else if (!Array.isArray(b)) {
+    console.log("Informe um array válido para o vetorB, por favor!");
+    TesteCondicional = true;
+  }
+
+  if (typeof taxaDeErro != "number") {
+    console.log("Informe um válor númerico para taxaDeErro, por favor!");
+    TesteCondicional = true;
+  }
+
+  if (typeof numeroMaxIteracoes != "number") {
+    console.log(
+      "Informe um válor númerico para numeroMaxIteracoes, por favor!"
+    );
+    TesteCondicional = true;
+  }
+
+  if (!Array.isArray(chuteInicial)) {
+    console.log("Informe um array para chuteInicial, por favor!");
+    TesteCondicional = true;
+  } else if (Array.isArray(chuteInicial)) {
+    for (let y = 0; y < A.length; y++) {
+      if (typeof chuteInicial[y] != "number") {
+        console.log(`O valor: ${chuteInicial[y]} não é um valor válido!`);
+        TesteCondicional = true;
+      }
+    }
+  }
+  if (TesteCondicional) {
+    return 0;
+  }
+
+  return 1;
+}
+
+module.exports = {
+  verificaInte
+};
